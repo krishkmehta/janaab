d4f9df505470feded58639990485a23a52648857
monu1691 <narayan1691.sk@gmail.com>
2020-02-05 23:57:02 +0530

    working header footer

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cpunks_janaab' );
+define( 'DB_NAME', 'janaab' );
 
 /** MySQL database username */
-define( 'DB_USER', 'cpunks_janaab' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Memory!@6989#' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
