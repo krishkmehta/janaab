5503f9ece549b71b17fcfa39fd88914e5d117dea
Monu1691 <narayan1691.sk@gmail.com>
2020-02-09 18:50:34 +0530

    working on home page sections

--- wp-content/themes/janaab-child/assets/js/custom.js
+++ wp-content/themes/janaab-child/assets/js/custom.js
@@ -1,12 +1,44 @@
-	jQuery(document).ready(function(){
-		jQuery('.search-btn').click(function(event){
-				event.stopPropagation();
-			 	jQuery(this).parent().toggleClass('search-active');
-		 	jQuery('#searchform').on('click', function(event){
-		 		event.stopPropagation();
-	 		});
-		 	jQuery('body').on('click', function(){
-				jQuery('.widget_product_search').removeClass('search-active');
-		 	});
-	 	});
-	})
+
+
+
+jQuery(document).ready(function($){
+	jQuery('.search-btn').click(function(event){
+		event.stopPropagation();
+		jQuery(this).parent().toggleClass('search-active');
+		jQuery('#searchform').on('click', function(event){
+			event.stopPropagation();
+		});
+		jQuery('body').on('click', function(){
+			jQuery('.widget_product_search').removeClass('search-active');
+		});
+	});
+
+
+
+	// slider
+	//inspirations-slider
+	$('.inspirations-slider').slick({		
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		autoplaySpeed: 3000,
+		arrows: true,
+		dots: false,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {				
+				slidesToShow: 4
+			}
+		},
+		{
+			breakpoint: 575,
+			settings: {
+				slidesToShow: 3
+			}
+		}
+		]
+	});
+})
--- wp-content/themes/janaab-child/template-homepage.php
+++ wp-content/themes/janaab-child/template-homepage.php
@@ -97,6 +97,73 @@ get_header(); ?>
 	</div><!-- col-full -->
 </section><!-- common-sec -->
 
+<section class="common-sec featured-patt-sec text-center">
+	<div class="col-full">
+		<div class="bg-blue pattern-books-bx bg-img" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/feature-patt-bg.jpg);">
+			<div class="sec-title-bx">
+				<span class="foc tp"></span>
+				<span class="foc bt"></span>
+				<h2 class="text-white">Featured Pattern Books</h2>
+				<p>Spoin eravida gibh sel velit tuctor aliquet. Aenean dyfi iysda asslicikoi tudin
+				lorem quis bibendum auctor</p>
+			</div>
+
+			<div class="link">
+				<a class="common-btn" href="javascript:void(0);">Know More</a>
+				<a class="common-btn" href="javascript:void(0);">Gallery</a>
+			</div>
+		</div>
+		
+	</div><!-- col-full -->
+</section><!-- common-sec -->
+
+<section class="common-sec all-new-sec">
+	<div class="col-full">
+
+	</div><!-- col-full -->
+</section><!-- common-sec -->
+
+<section class="common-sec text-center our-work-sec bg-img" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/our-work-bg.jpg);">
+	<div class="col-full">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="sec-title-bx">
+					<span class="foc tp"></span>
+					<span class="foc bt"></span>
+					<h2>Interested In Our Work?</h2>
+					<p>Spoin eravida gibh sel velit tuctor aliquet. Aenean dyfi iysda asslicikoi tudin
+					lorem quis bibendum auctor</p>
+
+					<div class="link">
+						<a class="common-btn" href="javascript:void(0);">Contact with Jnaab</a>
+					</div>
+				</div>
+			</div><!-- col -->
+
+			<div class="col-lg-6"></div><!-- col -->
+		</div><!-- row -->
+	</div><!-- col-full -->
+</section><!-- common-sec -->
+
+<section class="common-sec text-center inspirations-sec">
+	<div class="col-full">
+		<h2>Top custom design inspirations</h2>
+
+		<div class="inspirations-slider">
+			<?php for($in=1; $in<=10; $in++){ ?>
+				<div>
+					<div class="inspirations-bx">
+						<a href="javascript:void(0);">
+							<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/image111.jpg" alt="Image">
+						</a>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+		
+	</div><!-- col-full -->
+</section><!-- common-sec -->
+
 
 <div class="col-full">
 <?php /* ?>
